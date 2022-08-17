9844cf59ad19a0ff2b82a3f05be14bcb424a3600
Guilhermo Masid <guilhermomasid@gmail.com>
2022-08-17 13:58:30 -0300

    improving code

--- src/handle_keypress.c
+++ src/handle_keypress.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 14:35:12 by gmasid            #+#    #+#             */
-/*   Updated: 2022/08/17 13:28:38 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/08/17 13:56:24 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,39 @@ void	print_steps(t_game *game)
 	ft_printf("Steps: %d\n", game->moves_count);
 }
 
-int	collect_all_coins(t_game *game)
-{
-	return (game->collectibles_count == 0);
-}
-
 int	can_move(t_game *game, int x, int y)
 {
 	if (game->map[x][y] == '1')
 		return (0);
-	if (game->map[x][y] == 'E' && !collect_all_coins(game))
+	if (game->map[x][y] == 'E' && game->collectibles_count != 0)
 		return (0);
-	if (game->map[x][y] == 'C')
-		game->collectibles_count--;
-	if (game->map[x][y] == 'E' && collect_all_coins(game))
-		game->finish_game = 1;
 	return (1);
+}
+
+void	move_player(t_game *game, int new_x_pos, int new_y_pos)
+{
+	int	prev_x_pos;
+	int	prev_y_pos;
+
+	prev_x_pos = game->player.x_pos;
+	prev_y_pos = game->player.y_pos;
+	if (!can_move(game, new_x_pos, new_y_pos))
+		return ;
+	if (game->map[new_x_pos][new_y_pos] == 'C')
+		game->collectibles_count--;
+	if (game->map[new_x_pos][new_y_pos] == 'E')
+		game->finish_game = 1;
+	game->player.x_pos = new_x_pos;
+	game->player.y_pos = new_y_pos;
+	game->map[prev_x_pos][prev_y_pos] = '0';
+	if (!game->finish_game)
+		game->map[new_x_pos][new_y_pos] = 'P';
+	print_steps(game);
+	if (game->finish_game)
+	{
+		sucess_message("You win the game!");
+		sucess_message("You can now close the window!");
+	}
 }
 
 void	move(int keycode, t_game *game)
@@ -47,44 +64,20 @@ void	move(int keycode, t_game *game)
 	if (game->finish_game)
 		return ;
 	if (keycode == KEY_UP)
-	{
-		if (!can_move(game, px - 1, py))
-			return ;
-		print_steps(game);
-		game->map[px][py] = '0';
-		px--;
-		game->map[px][py] = 'P';
-	}
+		move_player(game, px - 1, py);
 	if (keycode == KEY_DOWN)
-	{
-		if (!can_move(game, px + 1, py))
-			return ;
-		print_steps(game);
-		game->map[px][py] = '0';
-		px++;
-		game->map[px][py] = 'P';
-	}
+		move_player(game, px + 1, py);
 	if (keycode == KEY_LEFT)
 	{
-		if (!can_move(game, px, py - 1))
-			return ;
-		print_steps(game);
 		mlx_destroy_image(game->mlx, game->person);
 		game->person = load_img(game, PERSON_LEFT);
-		game->map[px][py] = '0';
-		py--;
-		game->map[px][py] = 'P';
+		move_player(game, px, py - 1);
 	}
 	if (keycode == KEY_RIGHT)
 	{
-		if (!can_move(game, px, py + 1))
-			return ;
-		print_steps(game);
 		mlx_destroy_image(game->mlx, game->person);
 		game->person = load_img(game, PERSON_RIGHT);
-		game->map[px][py] = '0';
-		py++;
-		game->map[px][py] = 'P';
+		move_player(game, px, py + 1);
 	}
 }
 
